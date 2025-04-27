161b40fd95d90cfe22eb71ea26df8f50ceb93520
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-04-27 18:20:13 +0300

    No need for lazy loading of ConnectionsProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,10 @@
 "use client";
 
+import ConnectionsProvider from "@/context/ConnectionsContext";
 import FilesProvider from "@/context/FilesContext";
 import SWProvider from "@/context/SWContext";
 import { initLogRocket } from "@/utils/initLogRocket";
-import dynamic from "next/dynamic";
 import { useEffect } from "react";
-
-const ConnectionsProvider = dynamic(
-  () => import("@/context/ConnectionsContext"),
-  { ssr: false },
-);
 
 export default function RootProviders({
   children,
