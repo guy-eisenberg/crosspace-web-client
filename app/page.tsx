c6f10a4980cca129d2ee4091a0c58a415ac2bc1e
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-06-21 23:06:26 +0300

    Force space page to be dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { v4 } from "uuid";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const spaceId = v4();
 
